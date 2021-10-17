755008b1af071f994124adef030755a2a0e8c3fc
Satwikan <satwikanmol25@gmail.com>
2021-10-17 22:16:12 +0530

    team

---
+++ src/pages/admin/team.js
@@ -0,0 +1,48 @@
+import * as React from "react";
+import {
+  List,
+  Datagrid,
+  TextField,
+  Edit,
+  EditButton,
+  SimpleForm,
+  Create,
+  TextInput,
+  ImageInput,
+  ImageField,
+} from "react-admin";
+
+const TeamTitle = ({ record }) => {
+  return <span>Team member {record ? `"${record.name}"` : ""}</span>;
+};
+
+export const TeamList = (props) => (
+  <List {...props}>
+    <Datagrid rowClick="edit">
+      <TextField source="id" />
+      <TextField source="name" />
+      <EditButton />
+    </Datagrid>
+  </List>
+);
+
+export const TeamEdit = (props) => (
+  <Edit title={<TeamTitle />} {...props}>
+    <SimpleForm>
+      <TextInput disabled source="id" />
+      <TextInput source="name" />
+      <ImageField source="image" />
+    </SimpleForm>
+  </Edit>
+);
+
+export const TeamCreate = (props) => (
+  <Create title={<TeamTitle />} {...props}>
+    <SimpleForm>
+      <TextInput source="name" />
+      <ImageInput source="image" label="Images" accept="image/*">
+        <ImageField source="image" />
+      </ImageInput>
+    </SimpleForm>
+  </Create>
+);

01f232a303679da3e4308878922ef8adf2d540a4
anurag <anuraggupta3979@gmail.com>
2021-10-24 19:59:04 +0530

    home page refactor

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./pages/Home";
 import Categories from "./pages/Categories";
 import CategoryPage from "./pages/CategoryPage";
 import ProductPage from "./pages/ProductPage";
-// import AdminPage from "./pages/admin";
+import AdminPage from "./pages/admin";
 import { Switch, Route } from "react-router-dom";
 import categoryList from "./categoryList";
 import teamList from "./teamList";
